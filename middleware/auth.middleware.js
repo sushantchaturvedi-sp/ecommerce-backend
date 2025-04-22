22465bdb5c5666f66d525a5f4dac91b8f014499e
Sp-ishab <isha_bansal@softprodigy.com>
2025-04-22 18:55:24 +0530

    husky

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,48 +1,51 @@
-const jwt = require('jsonwebtoken')
-const User = require('../models/User.models')
-const asyncHandler = require('./async.middleware')
-const ErrorResponse = require('../utils/errorResponse.utils')
+const jwt = require('jsonwebtoken');
+const User = require('../models/User.models');
+const asyncHandler = require('./async.middleware');
+const ErrorResponse = require('../utils/errorResponse.utils');
 
 // Check user is logged in
-exports.protect = asyncHandler(async(req, res, next) => {
-    let token
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
 
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        // Set token from Bearer token in header
-        token = req.headers.authorization.split(' ')[1];
-    }else if(req.cookies.token) {
-        token = req.cookies.token
-    }
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    // Set token from Bearer token in header
+    token = req.headers.authorization.split(' ')[1];
+  }
 
-    // Make sure token exists
-    if(!token){
-        return next(new ErrorResponse('Please login to access',401))
-    }
+  // Make sure token exists
+  if (!token) {
+    return next(new ErrorResponse('Please login to access', 401));
+  }
 
-    try {
+  try {
     // Verify token
-    const decoded = await jwt.verify(token, process.env.JWT_SECRET)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = await User.findById(decoded.id)
+    req.user = await User.findById(decoded.id);
 
-    if(req.user.status === 'Banned'){
-        return next(new ErrorResponse('You are blocked by admin',401))
+    if (req.user.status === 'Banned') {
+      return next(new ErrorResponse('You are blocked by admin', 401));
     }
-    
-    next()
-    } catch(err){
-        return next(new ErrorResponse('Please login to access',401))
-    }
-})
 
+    next();
+  } catch (err) {
+    console.log(err);
+    return next(new ErrorResponse('Please login to access', 401));
+  }
+});
 
 // Admin authorization
 exports.authorize = (...roles) => {
-    return (req, res, next) => {
-        if(!roles.includes(req.user.role)){
-            return next(new ErrorResponse('Not Authorized to access this route',401))
-        }
-    
-        next()
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse('Not Authorized to access this route', 401)
+      );
     }
-}
+
+    next();
+  };
+};
