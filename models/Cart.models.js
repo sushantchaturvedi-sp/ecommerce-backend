3ed0a9192e9aafc4857eed29f8aacb98d9157466
sushantchaturvedi-sp <i.sush4nt@gmail.com>
2025-05-08 18:09:52 +0530

    removed commented code from cart model

--- models/Cart.models.js
+++ models/Cart.models.js
@@ -1,31 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const CartItemSchema = new mongoose.Schema({
-//   productId: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'Product',
-//     required: true,
-//   },
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   price: {
-//     type: Number,
-//     required: true,
-//   },
-//   quantity: { type: Number, default: 1 },
-//   // images: [{ type: String }],
-// });
-
-// const CartSchema = new mongoose.Schema({
-//   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-//   items: [CartItemSchema],
-//   createdAt: { type: Date, default: Date.now },
-// });
-
-// module.exports = mongoose.model('Cart', CartSchema);
-
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
