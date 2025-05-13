d3e739129f1847f7b129e506fc2b7d7d1f8ead83
sushantchaturvedi-sp <i.sush4nt@gmail.com>
2025-05-13 11:49:17 +0530

    Routes Changed to Noun from Verb

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -4,15 +4,19 @@ const router = express.Router();
 const cartController = require('../controllers/cart.Controller');
 
 // Add an item to the cart
-router.post('/add', cartController.addToCart);
+// router.post('/add', cartController.addToCart);
+router.post('/:userId/items', cartController.addToCart);
 
 // Remove an item from the cart
-router.delete('/remove/:userId/:productId', cartController.removeFromCart);
+// router.delete('/remove/:userId/:productId', cartController.removeFromCart);
+router.delete('/:userId/items/:productId', cartController.removeFromCart);
 
 // Get all items in the cart
-router.get('/:userId', cartController.getCart);
+// router.get('/:userId', cartController.getCart);
+router.get('/:userId/items', cartController.getCart);
 
 // Clear the cart
-router.delete('/clear/:userId', cartController.clearCart);
+// router.delete('/clear/:userId', cartController.clearCart);
+router.delete('/:userId/items', cartController.clearCart);
 
 module.exports = router;
