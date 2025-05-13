a16df99dd1eecef2bf17bde9f4e188139f92dcd8
sushantchaturvedi-sp <i.sush4nt@gmail.com>
2025-05-13 15:03:54 +0530

    Modified order controller to Delete Cart after successful order

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,19 +1,19 @@
 const asyncHandler = require('../middleware/async.middleware');
 const Order = require('../models/Order.models');
+const Cart = require('../models/Cart.models');
 
 // @desc    Place a new order
 // @route   POST /api/orders/checkout
 // @access  Private
+
 exports.placeOrder = asyncHandler(async (req, res) => {
-  const { items, paymentMethod } = req.body;
+  const { items, paymentMethod, shippingAddress } = req.body;
 
   if (!items || items.length === 0) {
     return res.status(400).json({ success: false, message: 'Cart is empty' });
   }
 
-  const user = req.user;
-
-  if (!user?.shippingAddress?.street) {
+  if (!shippingAddress?.street) {
     return res
       .status(400)
       .json({ success: false, message: 'Shipping address not found' });
@@ -25,12 +25,20 @@ exports.placeOrder = asyncHandler(async (req, res) => {
   );
 
   const order = await Order.create({
-    user: user._id,
+    user: req.user._id,
     items,
-    shippingAddress: user.shippingAddress,
+    shippingAddress,
     paymentMethod,
     totalAmount,
   });
+
+  // Clear the cart after successful order placement
+
+  const cart = await Cart.findOne({ user: req.user._id });
+  if (cart) {
+    cart.items = [];
+    await cart.save();
+  }
 
   res.status(201).json({
     success: true,
